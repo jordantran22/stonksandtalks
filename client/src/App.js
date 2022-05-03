5fc6381b94804dd5f86af2f8ce9d5097869f930d
Jordan Tran <jordan22tran@gmail.com>
2022-05-03 13:17:23 -0400

    testing websocket connection to yahoo
    retrieving live time price data from yahoo websocket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import protobuf from 'protobufjs';
+const { Buffer } = require('buffer/');
 
 function App() {
+  const [stonk, setStonk] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+    protobuf.load('./YPricingData.proto', (err, root) => {
+        if(err) {
+          return console.log(err);
+        }
+
+        const Yaticker = root.lookupType('yaticker');
+
+        ws.onopen = function open() {
+          console.log('connected');
+          ws.send(JSON.stringify({
+            subscribe: ['AMC']
+          }));
+        };
+      
+        ws.onclose = function close() {
+          console.log('disconnected');
+        };
+      
+        ws.onmessage = function incoming(message) {
+          console.log('comming message')
+          //console.log(data);
+          const next = (Yaticker.decode(new Buffer(message.data, 'base64')));
+          setStonk(next);
+        }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        stonk && 
+          <div>
+          {stonk.price}
+          </div>
+      }
     </div>
   );
 }
