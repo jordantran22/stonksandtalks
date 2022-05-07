5cf55aef1157e2496d03bf30c24d099f4a4c4ca0
Jordan Tran <jordan22tran@gmail.com>
2022-05-06 23:16:14 -0400

    implemented chat functionality, emit, receive

--- client/src/components/Chatroom.js
+++ client/src/components/Chatroom.js
@@ -1,20 +1,45 @@
 import React from 'react'
 import io from 'socket.io-client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const socket = io.connect(process.env.React_App_SERVER_URL);
 
 const Chatroom = ({ticker}) => {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [userConnectionToRoom, setUserConnectionToRoom] = useState(false);
 
   const joinRoom = () => {
     socket.emit("join_room", ticker.stockTicker);
+    setUserConnectionToRoom(true);
   }
 
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      console.log("sent message to server");
+      const messageData = {
+        room: ticker.stockTicker,
+        author: "gigachad",
+        message: currentMessage,
+        time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((messageList) => [...messageList, messageData]);
+    }
+  };
 
   useEffect(() => {
-    joinRoom();
-  }, []);
+    if(!userConnectionToRoom) {
+      joinRoom();
+    }
 
+    socket.on("receive_message", (data) => {
+      console.log(data);
+      setMessageList((messageList) => [...messageList, data]);
+      console.log(messageList)
+    });
+  }, [socket]);
 
   return (
     <div className='chatRoomContainer'>
@@ -25,11 +50,24 @@ const Chatroom = ({ticker}) => {
 
         <div className='messageContainer'>
             <div className='welcomeMessage'>Welcome to chat room!</div>
+            {
+              messageList.map((message) => {
+                return (
+                  <div>
+                    <div>{message.author}: </div>
+                    <div>{message.message}</div>
+                    <div>{message.time}</div>
+                  </div>
+                )
+              })
+            }
         </div>
 
         <div className='sendMessageContainer'>
-            <input type="text" className='chatbar'/>
-            <button className='chatButton'>Chat</button>
+            <input type="text" className='chatbar' onChange={(event) => {
+              setCurrentMessage(event.target.value);
+            }}/>
+            <button className='chatButton' onClick={() => sendMessage()}>Chat</button>
         </div>
 
     </div>
--- server/index.js
+++ server/index.js
@@ -23,7 +23,13 @@ io.on("connection", (socket) => {
     socket.on("join_room", (data) => {
         socket.join(data);
         console.log(`User with id: ${socket.id} joined room: ${data}`);
-    })
+    });
+
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("receive_message", data);
+        console.log("sent message back");
+    });
 
     socket.on("disconnect", () => {
         console.log("user disconnected", socket.id);
