80d2b2ddf8a8150f3a1991d28626979669ce29d8
Jordan Tran <jordan22tran@gmail.com>
2022-05-11 20:16:47 -0400

    news component link and CSS

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 
 const News = () => {
     const [news, setNews] = useState([]);
+    // const date = new Date(Date.now()).toJSON().substring(0,10);
+    const date = new Date(Date.now()).toDateString();
 
     const getNews = async () => {
         const requestInfo = {
@@ -30,9 +32,19 @@ const News = () => {
               
                 news.map((report) => {
                     return(
-                        <div>
-                            <h3>{report.articleHeader}</h3>
+                        <a href={report.link} target="_blank" style={{ textDecoration: 'none' }}>
+                        <div className='newsComponentContainer'>
+                             <img className='newsImage' src={report.img !== "none" ? 
+                                report.img : 
+                                'https://media.istockphoto.com/photos/price-of-btc-is-going-to-breakout-picture-id1310618429?b=1&k=20&m=1310618429&s=170667a&w=0&h=DsVwbbr3jKcS711y3l_-Bw-uX2RPC5apPM3FqUdNCU4=' 
+                            } />
+
+                            <div>
+                                <h3 className='articleHeader'>{report.articleHeader}</h3>
+                                <div className='newsDate'>{date}</div>
+                            </div>
                         </div>
+                        </a>
                     );
                 })
             }
